b3a04ff03ceae51d15c6fe3f97dbbe8ac6e8e4d2
Francis <fwang36@gmail.com>
2022-04-06 18:53:15 -0400

    add instanced variables to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['NAME'], password: ENV['PASS']
   def show
-
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
+
