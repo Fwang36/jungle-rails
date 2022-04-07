8a936bb14406190bddd3f70574febc33b6967e98
Francis <fwang36@gmail.com>
2022-04-07 15:46:42 -0400

    add categories list and categories navbar button

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,22 @@
 class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV['NAME'], password: ENV['PASS']
-  
+
   def index
+    @categories = Category.all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end  
 
 end
