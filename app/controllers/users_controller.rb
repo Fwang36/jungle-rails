cbe38baa28eb2152549a88c3620604498ed1af50
Francis <fwang36@gmail.com>
2022-04-07 16:49:36 -0400

    add login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,9 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    
+
     if user.save
-      session[:user_id] = user.index
+      session[:user_id] = user.id
       redirect_to '/'
     else
       redirect_to '/signup'
