c68f2d5722a94cfc4a666e858d46910232192701
Francis <fwang36@gmail.com>
2022-04-11 17:53:10 -0400

    add password confirmation test

--- spec/models/user_specs_spec.rb
+++ spec/models/user_specs_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe UserSpecs, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :model do
+
+  describe "Validations" do
+
+    before do
+      @user = User.new(
+        email: "test@123.com", 
+        name: "test_user", 
+        password: "test", 
+        password_confirmation: "test"
+      )
+    end
+    it "is a valid user" do
+      expect(@user.valid?).to be true
+    end
+
+    it "validates password and password confirmation match" do
+      @user.password_confirmation = "tet"
+
+      expect(@user.valid?).to be false
+    end
+
+  end
 end
